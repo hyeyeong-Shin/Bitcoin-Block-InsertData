c16282b29fb5b96a38544ce66ffffbce7b0abccf
Hye-yeong Shin <86094244+hyeyeong-Shin@users.noreply.github.com>
2021-06-21 16:38:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from module import insertBlock_
 
 def main():
     start = check_DB_bestBlockHeight.database_bestBlockHeight() 
-    #start = 386780
     end = getbestblock.bitcoin_getBestBlock()
-    #end = 100000
     
     if((start<end)and(start!=end)): 
         insertBlock_.insert_blockData(start, end)
@@ -23,7 +21,7 @@ if __name__=="__main__":
         now = time.localtime()
         print()
         print('%04d/%02d/%02d %02d:%02d:%02d'% (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
-        time.sleep(60)
+        time.sleep(60*60)
 
         now = time.localtime()
         print('%04d/%02d/%02d %02d:%02d:%02d'% (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
